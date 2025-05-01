731f7f245316ef8743c47ee34b07ab187fa81fa5
Jamil <gafforovabdujamil@gmail.com>
2025-05-01 17:13:23 +0500

    btns: border & animation refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -271,11 +271,10 @@ export default function Home() {
                                         cursor-pointer w-full !h-[51px] !rounded-[4px] group flex items-center !justify-between`}
                                             style={{
                                                 color: activeTab === 'contact' ? "#3D9ED6" : "#878787",
-                                                border: activeTab === 'contact' ? "1px solid rgb(204 204 204 / 70%)" : "1px solid #353535",
                                             }}
                                         >
                                             <svg
-                                                className={`${styles.sendIconLeft}  transition-all duration-200 ease-in`}
+                                                className={`${styles.sendIconLeft}  transition-all !duration-[.15s] ease-in`}
                                                 width="30" height="17" viewBox="0 0 30 17" fill="none"
                                                 xmlns="http://www.w3.org/2000/svg">
                                                 <path
@@ -289,11 +288,11 @@ export default function Home() {
                                                     fill={activeTab === 'contact' ? "#ccc" : "#737373"}/>
                                             </svg>
                                             <span
-                                                className="text-[20px] !transition-all !duration-200 !ease-in !group-hover:text-[#ccc]">
+                                                className="text-[20px] !transition-all !duration-[.15s] !ease-in !group-hover:text-[#ccc]">
                                           Связаться
                                         </span>
                                             <svg
-                                                className={`${styles.sendIconRight}  transition-all duration-200 ease-in`}
+                                                className={`${styles.sendIconRight}  transition-all !duration-[.15s] ease-in`}
                                                 width="30" height="17" viewBox="0 0 30 17" fill="none"
                                                 xmlns="http://www.w3.org/2000/svg">
                                                 <path
@@ -316,22 +315,21 @@ export default function Home() {
                                             className={`${styles["btn"]} ${HeaderStyles["login-button"]} cursor-pointer w-full !h-[51px] !rounded-[4px] group flex items-center !justify-between `}
                                             style={{
                                                 color: activeTab === 'requisite' ? "#3D9ED6" : "#878787",
-                                                border: activeTab === 'requisite' ? "1px solid rgb(204 204 204 / 70%)" : "1px solid #353535",
                                             }}
                                         >
                                             <svg
-                                                className={`${styles.sendIconLeft}  transition-all duration-300 ease-in`}
+                                                className={`${styles.sendIconLeft}  transition-all !duration-[.15s] ease-in`}
                                                 width="24" height="27" viewBox="0 0 24 27" fill="none"
                                                 xmlns="http://www.w3.org/2000/svg">
                                                 <path
                                                     d="M13.3929 7.38281V0H5.85938C5.19337 0 4.55465 0.259277 4.08372 0.720792C3.61278 1.18231 3.34821 1.80826 3.34821 2.46094V14.7656H12.5558C13.2218 14.7656 13.8605 15.0249 14.3315 15.4864C14.8024 15.9479 15.067 16.5739 15.067 17.2266C15.067 17.8792 14.8024 18.5052 14.3315 18.9667C13.8605 19.4282 13.2218 19.6875 12.5558 19.6875H3.34821V23.7891C3.34821 24.4417 3.61278 25.0677 4.08372 25.5292C4.55465 25.9907 5.19337 26.25 5.85938 26.25H20.9263C21.5923 26.25 22.2311 25.9907 22.702 25.5292C23.1729 25.0677 23.4375 24.4417 23.4375 23.7891V9.84375H15.904C15.238 9.84375 14.5993 9.58447 14.1284 9.12296C13.6574 8.66144 13.3929 8.03549 13.3929 7.38281ZM6.69643 12.3047C6.69643 12.0871 6.78462 11.8785 6.9416 11.7246C7.09857 11.5708 7.31148 11.4844 7.53348 11.4844H19.2522C19.4742 11.4844 19.6871 11.5708 19.8441 11.7246C20.0011 11.8785 20.0893 12.0871 20.0893 12.3047C20.0893 12.5222 20.0011 12.7309 19.8441 12.8847C19.6871 13.0386 19.4742 13.125 19.2522 13.125H7.53348C7.31148 13.125 7.09857 13.0386 6.9416 12.8847C6.78462 12.7309 6.69643 12.5222 6.69643 12.3047ZM6.69643 22.1484C6.69643 21.9309 6.78462 21.7222 6.9416 21.5684C7.09857 21.4146 7.31148 21.3281 7.53348 21.3281H19.2522C19.4742 21.3281 19.6871 21.4146 19.8441 21.5684C20.0011 21.7222 20.0893 21.9309 20.0893 22.1484C20.0893 22.366 20.0011 22.5746 19.8441 22.7285C19.6871 22.8823 19.4742 22.9688 19.2522 22.9688H7.53348C7.31148 22.9688 7.09857 22.8823 6.9416 22.7285C6.78462 22.5746 6.69643 22.366 6.69643 22.1484ZM15.067 7.38281V0.410156L23.019 8.20312H15.904C15.682 8.20312 15.4691 8.1167 15.3121 7.96286C15.1552 7.80902 15.067 7.60037 15.067 7.38281ZM0.837054 16.4062C0.615053 16.4062 0.402145 16.4927 0.245167 16.6465C0.0881895 16.8004 0 17.009 0 17.2266C0 17.4441 0.0881895 17.6528 0.245167 17.8066C0.402145 17.9604 0.615053 18.0469 0.837054 18.0469H12.5558C12.7778 18.0469 12.9907 17.9604 13.1477 17.8066C13.3047 17.6528 13.3929 17.4441 13.3929 17.2266C13.3929 17.009 13.3047 16.8004 13.1477 16.6465C12.9907 16.4927 12.7778 16.4062 12.5558 16.4062H0.837054Z"
                                                     fill={activeTab === 'requisite' ? "#ccc" : "#737373"}/>
                                             </svg>
-                                            <span className="text-[20px] !transition-all !duration-200 !ease-in">
+                                            <span className="text-[20px] !transition-all !duration-[.15s] !ease-in">
                                           Реквизиты
                                         </span>
                                             <svg
-                                                className={`${styles.sendIconRight}  transition-all duration-300 ease-in`}
+                                                className={`${styles.sendIconRight}  transition-all !duration-[.15s] ease-in`}
                                                 width="24" height="27" viewBox="0 0 24 27" fill="none"
                                                 xmlns="http://www.w3.org/2000/svg">
                                                 <path
@@ -341,7 +339,6 @@ export default function Home() {
                                         </button>
                                         <div className={styles.highlight}/>
                                     </div>
-
 
                                 </div>
                             </div>
@@ -526,7 +523,7 @@ export default function Home() {
                                                         data-text=""
                                                     >
                                                         <svg
-                                                            className={`${styles.sendIconLeft} transition-all duration-200 ease-in`}
+                                                            className={`${styles.sendIconLeft} transition-all !duration-[.15s] ease-in`}
                                                             width="23" height="20" viewBox="0 0 23 20" fill="none"
                                                             xmlns="http://www.w3.org/2000/svg">
                                                             <path
@@ -534,11 +531,11 @@ export default function Home() {
                                                                 fill="#CCCCCC"/>
                                                         </svg>
                                                         <span
-                                                            className="!transition-all !duration-200 !ease-in font-normal text-[18px] leading-[120%]">
+                                                            className="!transition-all !duration-[.15s] !ease-in font-normal text-[20px] leading-[120%]">
                                                               Отправить
                                                             </span>
                                                         <svg
-                                                            className={`${styles.sendIconRight}  transition-all duration-200 ease-in`}
+                                                            className={`${styles.sendIconRight}  transition-all !duration-[.15s] ease-in`}
                                                             width="23" height="20" viewBox="0 0 23 20" fill="none"
                                                             xmlns="http://www.w3.org/2000/svg">
                                                             <path
@@ -606,7 +603,7 @@ export default function Home() {
                                                 alt="pdf-icon"
                                             />
                                             <span
-                                                className="!transition-all !duration-200 !ease-in !group-hover:text-[#ccc]">
+                                                className="!transition-all !duration-200 !ease-in !group-hover:text-[#ccc] text-[20px]">
                                                           Скачать PDF
                                                         </span>
                                             <Image
