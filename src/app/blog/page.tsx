828a3dcd820b654ddf9b020e500baa0da9fd3547
Jamil <gafforovabdujamil@gmail.com>
2025-05-10 18:47:27 +0500

    page contact

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "@/components/header/Header";
 import styles from "@/app/page.module.scss";
 import Footer from "@/app/footer";
+import ContactsCard from '@/components/ContactsCard/ContactsCard'
 
 const BlogPage = () => {
     return (
@@ -13,11 +14,35 @@ const BlogPage = () => {
             >
                 <div className={`${styles.linear}  absolute inset-0 bg-black/20`}></div>
             </div>
+            <div
+                className={`${styles.blog} w-full min-h-full h-auto mx-auto flex flex-col items-center`}>
             <Header/>
-                <div className={`${styles.blog} w-full max-w-[1180px] my-[10px] h-full mt-[60px] mb-[100px] mx-auto flex flex-col items-center`}>
-                    <h2 className={`${styles.txtGradientRight} text-center text-[56px] leading-[110%] mb-10`}>Полезные статьи и советы по <br/> преобразованию аудио в текст</h2>
+                <div className={`${styles.blog} w-full max-w-[1180px] my-[10px] min-h-full h-auto mt-[60px] mb-[127px] mx-auto flex flex-col items-center`}>
+                    <h2 className={`${styles.txtGradientRight} text-center text-[56px] leading-[110%] mb-10`}>Полезные
+                        статьи и советы по <br/> преобразованию аудио в текст</h2>
+
+                    <div className={`${styles.blogCardsContainer} h-auto w-full`}>
+                        <div className={`${styles.blogCards} grid grid-cols-4 gap-[40px] mb-[127px]`}>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                            <ContactsCard/>
+                        </div>
+                    </div>
                 </div>
-            <Footer/>
+            </div>
         </div>
     );
 };
---
+++ src/components/ContactsCard/ContactsCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import HeaderStyles from '../../components/header/Header.module.css'
+import styles from "@/app/page.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+const ContactsCard = () => {
+    return (
+        <div
+            className={`${styles.card} group w-full h-[348px]; max-w-[260px] border border-[#353535] hover:border-[#CCCCCC] active:scale-[98%] rounded-[8px] overflow-hidden transition-border duration-200 ease-in`}>
+            <div className={`w-full h-[171px] rounded-tl-[8px] rounded-tr-[8px] relative`}>
+                <Image
+                    src="/image.png"
+                    alt="img"
+                    width={260}
+                    height={171}
+                    className={`min-h-[171px] rounded-tl-[8px] rounded-tr-[8px] object-cover relative top-[-2] group-hover:scale-105 transition-scale duration-200 ease-in`}
+                />
+            </div>
+
+            <div className={`${styles.cardBody} py-[20px] px-[15px]`}>
+                <h3 className={`${styles.cardTitle} text-[20px] text-[#CCCCCC] leading-[120%] mb-6 group-hover:text-[#3D9ED6] transition-all duration-200 ease-in`}>Транскрибация
+                    вакансии без опыта работы для школьников</h3>
+
+                <div className={`${styles.cardBody} w-full flex  items-end justify-between`}>
+                    <div className="relative">
+                        <Link href={`/#`}
+                              className={`${HeaderStyles["login-button"]} group flex items-center justify-center`}
+                              data-text="">
+                                            <span className="text-[18px] leading-[120%]">
+                                              Подробнее
+                                            </span>
+                        </Link>
+                        <div className={styles.highlight}/>
+                    </div>
+
+                    <p className={`${styles.cardBodyDate} text-[#737373] text-[10.6641px] leading-[13px]`}>10.10.2024</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactsCard;
