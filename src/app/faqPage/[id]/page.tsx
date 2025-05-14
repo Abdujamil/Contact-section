0867b59b3c32675ce4580b9c4ddc6ee2073e95de
Jamil <gafforovabdujamil@gmail.com>
2025-05-14 10:45:14 +0500

    faqPage refactor v2

--- src/app/faqPage/[id]/page.tsx
+++ src/app/faqPage/[id]/page.tsx
@@ -1,16 +1,15 @@
-import { notFound } from "next/navigation";
-import { faqData } from "@/data/faq";
+import {notFound} from "next/navigation";
+import {faqData} from "@/data/faq";
 import React from "react";
 import Footer from "../../footer";
 import FaqPageContent from "@/components/FaqPageCard/FaqPageContent";
 import Header from "@/components/header/Header";
 import Bg from "@/components/background/bg";
 
-interface PageProps {
-    params: {
-        id: string;
-    };
-}
+type Props = {
+    // params: { id: string };
+    params: Promise<{ id: string }>;
+};
 
 export async function generateStaticParams() {
     return faqData.map((item) => ({
@@ -18,20 +17,25 @@ export async function generateStaticParams() {
     }));
 }
 
-export default async function FaqPage({ params }: PageProps) {
-    const id = parseInt(params.id);
+
+export default async function FaqPage({params}: Props) {
+    const {id: idString} = await params;
+    const id = parseInt(idString);
     const faqItem = faqData.find((item) => item.id === id);
 
     if (!faqItem) return notFound();
 
     return (
-        <div className={`h-dvh`}>
-            <Bg />
-            <Header />
-            <div className="w-full max-w-[1180px] h-auto min-h-lvh mx-auto mt-[60px] px-[10px] mb-[100px] grid grid-cols-4 gap-[58px]">
-                <FaqPageContent id={id} />
+        <>
+            <div className={`h-dvh`}>
+                <Bg/>
+                <Header/>
+                <div
+                    className="w-full max-w-[1180px] h-auto min-h-lvh mx-auto mt-[60px] px-[10px] mb-[100px] grid grid-cols-4 gap-[58px]">
+                    <FaqPageContent id={id}/>
+                </div>
+                <Footer/>
             </div>
-            <Footer />
-        </div>
+        </>
     );
 }
