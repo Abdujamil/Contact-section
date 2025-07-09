284145cf9d57d361fbca43052ef0231e1b53b0c3
A G <ag@As-MacBook-Air.local>
2025-07-09 21:02:50 +0500

    login form: height fix

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -115,7 +115,7 @@ export default function AuthLayout({children}: { children: ReactNode }) {
                     </aside>
 
                     <div
-                        className={`${styles.BlogPageContent} h-full text-[18px] leading-relaxed whitespace-pre-line p-[30px] border border-[#353535] rounded-[6px]`}
+                        className={`${styles.BlogPageContent} h-full md:h-[579px] text-[18px] leading-relaxed whitespace-pre-line p-[30px] border border-[#353535] rounded-[6px]`}
                     >
                         {children}
                     </div>
