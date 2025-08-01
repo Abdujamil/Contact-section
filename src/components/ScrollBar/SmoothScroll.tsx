69f53c2cb1a2215bdfb42387d4708db90d87d8df
Abdujamil <suhrobgafarov3887@gmail.com>
2025-08-01 16:15:56 +0500

    scrollSmooth: fix err

--- src/components/ScrollBar/SmoothScroll.tsx
+++ src/components/ScrollBar/SmoothScroll.tsx
@@ -1989,7 +1989,7 @@ export default function SmoothScroll({children}: SmoothScrollProps) {
             {showScrollbar && <div ref={scrollbarRef} className="scrollbar md:block hidden"></div>}
 
             {/* ===== LIVE SETTINGS PANEL ===== */}
-            {process.env.NODE_ENV === 'production' (
+            {process.env.NODE_ENV === 'production' && (
                 <div
                     className="fixed top-[70px] right-4 backdrop-blur-2xl border border-gray-300 dark:border-gray-600 rounded-lg shadow-lg p-4 z-[9999999999] w-80 max-h-[80vh] overflow-y-auto allow-native-scroll">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">
