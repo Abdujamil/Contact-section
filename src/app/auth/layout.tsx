11c6bd904e515def04b0b78fc49198d40dd84c89
Abdujamil <suhrobgafarov3887@gmail.com>
2025-09-02 22:42:08 +0500

    auth refactor style v4

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -71,7 +71,10 @@ export default function AuthLayout({children}: { children: ReactNode }) {
                     <div className={`md:flex gap-[40px] md:pt-[60px] md:pb-[60px]`}>
                         <aside className="md:w-[260px] w-[320px] m-auto md:m-0 flex flex-col items-start mt-25 mb-5">
                             <h2
-                                className={`${styles.txtGradientRight} hidden md:block leading-[80%] md:text-[48px] text-[28px] md:text-left text-center font-normal md:mb-[27px] mb-[20px] mt-[-1px]`}
+                                className={`${styles.txtGradientRight} 
+                                hidden md:block leading-[80%] md:text-[48px] text-[28px] md:text-left text-center 
+                                font-normal md:mb-[27px] mb-[20px] mt-[-1px] tracking-[-1.2px]
+                                `}
                             >
                                 Вход
                             </h2>
