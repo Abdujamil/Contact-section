5171e54520f2c884104718eeed6366d1b381ec90
Abdujamil <suhrobgafarov3887@gmail.com>
2025-09-16 23:54:36 +0500

    add manifest.json & header position on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type {Metadata} from "next";
+import type {Metadata, Viewport} from "next";
 // import {Rubik} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
@@ -38,6 +38,23 @@ import Script from "next/script";
 export const metadata: Metadata = {
     title: "Audiosector",
     description: "Магазин профессионального аудиооборудования",
+    manifest: "/manifest.json",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "black-translucent",
+        title: "Audiosector",
+    },
+    // icons: {
+    //     icon: [
+    //         { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+    //         { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    //     ],
+    //     apple: "/icons/icon-192x192.png",
+    // },
+};
+
+export const viewport: Viewport = {
+    themeColor: "#0d0d0d",
 };
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
